893a0ffd9f207b2b52107862b982a038288b6bd6
Arek Kwapis <arek.kwapis@gmail.com>
2023-02-18 18:34:13 +0000

    Test script so it stops failing constantly

--- index.js
+++ index.js
@@ -1,2 +1,15 @@
-const colors = require('colors')
+const errorHandler = require('./src/modules/error-handler');
+var errorOut = errorHandler(0, 'Something went wrong.', 'server.js');
+console.log(errorOut);
 
+var errorOut = errorHandler(1, 'Something went wrong.', 'server.js');
+console.log(errorOut);
+
+var errorOut = errorHandler(2, 'Something went wrong.', 'server.js');
+console.log(errorOut);
+
+var errorOut = errorHandler(3, 'Something went wrong.', 'server.js');
+console.log(errorOut);
+
+var errorOut = errorHandler(4, 'Something went wrong.', 'server.js');
+console.log(errorOut);
