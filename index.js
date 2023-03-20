701e4faf6ed44b245c2e6fabe46644fba159f00f
Arek Kwapis <arek.kwapis@gmail.com>
2023-03-20 22:50:34 +0000

    Update comments

--- index.js
+++ index.js
@@ -3,10 +3,12 @@ const logMsg = loggingUtils.errorHandler
 
 const test = require('./src/modules/startup');
 
+//test cases
 logMsg(0, 'Something went wrong.', 'server.js');
 logMsg(1, 'Something went wrong.', 'server.js');
 logMsg(2, 'Something went wrong.', 'server.js');
 logMsg(3, 'Something went wrong.', 'server.js');
 logMsg(4, 'Something went wrong.', 'server.js');
 
+//required for debug of startup functions
 test
