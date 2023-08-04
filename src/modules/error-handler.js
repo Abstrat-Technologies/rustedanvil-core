4b86ed1b46f90fdfbda3435d18d64c80b97cf989
Arek Kwapis <arek.kwapis@gmail.com>
2023-08-04 19:21:26 +0100

    fix dependancy issues for errorHandler

--- src/modules/error-handler.js
+++ src/modules/error-handler.js
@@ -13,6 +13,8 @@
 
 const colours = require('colors')
 const date = require('date-and-time')
+const path = require('path');
+const curFile = path.basename(__filename);
 
 /**
  * Quickly allows the current date, time and timezone to
@@ -69,8 +71,8 @@ function errorLevelFormat(errorLevel) {
 }
 
 /**
- * Creates a nicley formatted log message ready for console, 
- * with date, time, error level, message, and location
+ * Creates a nicley formatted log message ready for console, with date,
+ * time, error level, message, and location
  * 
  * 0 = INFO;
  * 1 = DEBUG;
@@ -85,7 +87,7 @@ function errorLevelFormat(errorLevel) {
  * 
  * @returns {string} the formatted message ready to be printed
  */
-function errorHandler(errorLevel, errorMsg, errorLocation, outputFunc = outputMsg) {
+function consoleOut(errorLevel, errorMsg, errorLocation, outputFunc = outputMsg) {
 	dateOut = dateOutput()
 	formattedErrorLevel = errorLevelFormat(errorLevel)
 	let errorOut = `${colours.gray(dateOut)} ${formattedErrorLevel}${colours.gray(": ")}${colours.brightBlue(errorMsg)}${colours.gray(" - called by ")}${colours.brightCyan(errorLocation)}`
@@ -103,7 +105,69 @@ function outputMsg(msg) {
 	console.log(msg)
 }
 
+/** 
+ * Outputs an error relevant to local read operations from fs.readFile()
+ * 
+ * @param {string} err the error produced
+ * @param {boolean} isCritical was the operation required to proceed
+ * 
+ * @returns {boolean} is it okay to proceed
+ */
+function readLocalFileError(err, isCritical) {
+
+
+
+	switch (err) {
+		// Permission denied
+		case 'EACCES':
+			consoleOut(4, "Read file error: Permission denied. (RA-1001)", curFile)
+			criticalCheck(isCritical)
+			break
+		// Bad file descriptor
+		case 'EBADF':
+			consoleOut(4, "Read file error: Bad file descriptor. (RA-1002)", curFile)
+			break
+		// Too many open files
+		case 'EMFILE':
+			consoleOut(4, "Read file error: Too many open files. (RA-1003)", curFile)
+			break
+		// No such file or directory
+		case 'ENOENT':
+			consoleOut(4, "Read file error: No such file or directory. (RA-1004)", curFile)
+			break
+		// Not a directory
+		case 'ENOTDIR':
+			consoleOut(4, "Read file error: Not a directory. (RA-1005)", curFile)
+			break
+		// File name too long
+		case 'ENAMETOOLONG':
+			consoleOut(4, "Read file error: File name is too long. (RA-1006)", curFile)
+			break
+		// Is a directory
+		case 'EISDIR':
+			consoleOut(4, "Read file error: Expected file, is a directory. (RA-1007)", curFile)
+			break
+		// Input/output error
+		case 'EIO':
+			consoleOut(4, "Read file error: I/O error. (RA-1008)", curFile)
+			break
+		default:
+			consoleOut(4, "Read file error: Unknown error. (RA-1010)", curFile)
+			break
+	}
+
+	function criticalCheck(isCritical) {
+		if (isCritical = 1) {
+			return false
+			// Proceed to exit handler
+		} else {
+			return true
+		}
+	}
+}
+
 module.exports = {
-	errorHandler,
-	outputMsg
+	consoleOut,
+	outputMsg,
+	readLocalFileError
 }
